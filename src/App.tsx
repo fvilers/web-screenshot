47e2421843719d920eb8fc0df6c46bb686f12e80
Fabian Vilers <fabian.vilers@dev-one.com>
2021-02-15 07:21:28 +0100

    Remove default

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Screenshot</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        reprehenderit nam nesciunt distinctio asperiores fuga officia veniam
+        odit perspiciatis ullam, dignissimos, quasi earum. Hic consectetur
+        architecto perferendis iste vel commodi.
+      </p>
     </div>
   );
 }
